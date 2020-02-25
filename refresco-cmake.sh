335720ce7435543ccc4b343d82c289ed699a83ac
nanoseb <sebastien.lemaire@soton.ac.uk>
2020-02-25 12:17:19 +0000

    update cmake script for overset compilation

--- refresco-cmake.sh
+++ refresco-cmake.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CMAKE_FOLDER=cmake-trunk
-SOURCE_DIR=$HOME/ReFRESCO/Dev/trunk
-# CMAKE_FOLDER=cmake-overset
-# SOURCE_DIR=$HOME/ReFRESCO/Dev/branch/overset
+# CMAKE_FOLDER=cmake-trunk-gnu
+# SOURCE_DIR=$HOME/ReFRESCO/Dev/trunk
+CMAKE_FOLDER=cmake-overset
+SOURCE_DIR=$HOME/ReFRESCO/Dev/branches/overset
 
 REFRESCO_INSTALL_DIR=$HOME/ReFRESCO/$CMAKE_FOLDER/install
 REFRESCO_BUILD_DIR=$HOME/ReFRESCO/$CMAKE_FOLDER/build-refresco
@@ -15,14 +15,25 @@ mkdir -p $EXTLIBS_BUILD_DIR
 
 # Build ext lib
 cd $EXTLIBS_BUILD_DIR 
-cmake $SOURCE_DIR/SuperBuild -Dsuperbuild_install_location=$REFRESCO_INSTALL_DIR/extLibs
+cmake $SOURCE_DIR/SuperBuild -Dsuperbuild_install_location=$REFRESCO_INSTALL_DIR/extLibs \
+                             -DENABLE_suggar=ON \
+                             -DENABLE_p3d=ON \
+                             -DENABLE_dirtlib=ON \
+                             -DENABLE_licensing=OFF \
+                             -DENABLE_paraview=OFF \
+                             -DENABLE_pfunit=ON \
+                             -DENABLE_xri=OFF
 make refrescolibs 
-
 
 # Build ReFRESCO
 cd $REFRESCO_BUILD_DIR
-cmake $SOURCE_DIR -DCMAKE_INSTALL_PREFIX=$REFRESCO_INSTALL_DIR -DREFRESCO_EXTLIBS_DIR=$REFRESCO_INSTALL_DIR/extLibs
-make -j 16 install_code install_tools
+cmake $SOURCE_DIR -DCMAKE_INSTALL_PREFIX=$REFRESCO_INSTALL_DIR -DREFRESCO_EXTLIBS_DIR=$REFRESCO_INSTALL_DIR/extLibs \
+                  -DENABLE_COPROCESSING=OFF \
+                  -DENABLE_LICENSING=OFF \
+                  -DENABLE_XMF=OFF \
+                  -DENABLE_OVERSET=ON
+
+# make -j 8 install_code install_tools
 
 
 echo
